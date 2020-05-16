eba2d4cbd8c6418f75c75f8fa1fb546675ea2ec7
Andrew <37120451+pumpykyt@users.noreply.github.com>
2020-05-16 14:08:36 +0300

    All project ready
    My course project on Angular TS + ASP.NET Core Api

--- Project P34.API+Angular/ClientApp/src/app/Areas/admin-area/Models/comment.model.ts
+++ Project P34.API+Angular/ClientApp/src/app/Areas/admin-area/Models/comment.model.ts
@@ -1,11 +1,9 @@
 export class CommentModel{
-    public Id: string;
     public text: string;
     public ArticleId: number;
     public userId: string;
 
     public constructor(){
-        this.Id = null;
         this.text = null;
         this.ArticleId = null;
         this.userId = null;
